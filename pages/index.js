f42e3cb1f988f4402163e7f6115a6cf2ab968d52
Iambizi <amirbzmn@gmail.com>
2021-09-12 14:51:31 -0400

    comments out api call and song name unordered list

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,30 @@
 import Head from 'next/head';
 import styles from '../styles/scss/styles.module.scss';
-import TiltCover from '../src/tilt.jsx';
-import Player from  '../src/player.jsx';
+import TiltCover from '../src/components/tilt.jsx';
+import Player from  '../src/components/player.jsx';
 import React, {useState, useEffect } from 'react';
+import { Credentials } from '../src/keys/credentials.js';
+import axios from 'axios';
 
-const defaultEndPoint = 'https://api.spotify.com/token';
-
-
-export async function getServerSideProps(){
-
-  const res = await fetch(defaultEndPoint);
-  const data = await res.json();
-  return{
-    props:{
-      data
-    }
-  }
-}
-
-export default function Home({data}) {
-  console.log(data);
+export default function Home() {
+  const spotify = Credentials();
+  // useEffect(()=>{
+  //   axios(`https://accounts.spotify.com/api/token`,{
+  //     headers: {
+  //       'content-Type': 'application/x-www-form-urlencoded',
+  //       'Authorization': 'Basic' + btoa(spotify.ClientId + ':' + spotify.ClientSecret)
+  //     },
+  //     data: 'grant_type-client_credentials',
+  //     method: 'POST'
+  //   })
+  //     .then(tokenResponse =>{
+  //       console.log(tokenResponse.data.access_token);
+  //       setToken(tokenResponse.data.access_token);
+  //     })
+    
+  // },[]);
+  console.log(process.env.NEXT_PUBLIC_CLIENT_ID);
+  
   return (
     <div className={styles.container}>
       <Head>
